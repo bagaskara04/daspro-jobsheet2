0c058f27615bf25b6fb245e51d0864a18ff6ad03
karalion9 <sagabdespair01@gmail.com>
2023-09-26 09:00:14 +0700

    commit contvariabel

--- MyFirstJavaGithub.java
+++ MyFirstJavaGithub.java
@@ -2,8 +2,7 @@
  * MyFirstJavaGithub
  */
 public class MyFirstJavaGithub {
-
     public static void main(String[] args) {
-        System.out.println("Hello jon");
+        System.out.println("Hello from Github");
     }
 }
